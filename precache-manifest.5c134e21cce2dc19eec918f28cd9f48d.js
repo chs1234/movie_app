c122ebdf391b3384ee369df591d3c3c2f7fa0218
Chae HeeSeung <chs4413@gmail.com>
2020-08-17 21:24:30 +0900

    Updates

---
+++ precache-manifest.5c134e21cce2dc19eec918f28cd9f48d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ec22d70900e87d80e41be5eab0584b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "a37a877e79dac80b90e2",
+    "url": "/movie_app/static/css/main.906608f0.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37a877e79dac80b90e2",
+    "url": "/movie_app/static/js/main.30544b59.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
